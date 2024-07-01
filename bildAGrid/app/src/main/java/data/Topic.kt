efa88b44a55ba7393054f2024d9ef0c32505ff47
m2margaret <margaritanenarokova@gmail.com>
2024-07-02 01:52:08 +0300

    unit3 practice

---
+++ bildAGrid/app/src/main/java/data/Topic.kt
@@ -0,0 +1,10 @@
+package data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
